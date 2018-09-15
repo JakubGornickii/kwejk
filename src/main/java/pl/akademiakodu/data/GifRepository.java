229f443e6d9df6a5a4199e27c960a03e81b79f42
Jakub <Blade123164@gmail.com>
2018-09-15 15:18:12 +0200

    first1

---
+++ src/main/java/pl/akademiakodu/data/GifRepository.java
@@ -0,0 +1,22 @@
+package pl.akademiakodu.data;
+
+import pl.akademiakodu.model.Gif;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GifRepository {
+
+
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+
+            new Gif("android-explosion","michalos",true),
+            new Gif("ben-and-mike","mike",true),
+            new Gif("book-dominos","ola",true),
+            new Gif("compiler-bot","bot",true),
+            new Gif("cowboy-coder","coder",true),
+            new Gif("infinite-andrew","andrew",true)
+
+    );
+}

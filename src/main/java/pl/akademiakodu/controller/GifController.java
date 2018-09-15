8d61693c28d9449dcd0f4c21413d38207c836a71
Jakub <Blade123164@gmail.com>
2018-09-15 16:30:29 +0200

    first1

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -1,29 +1,47 @@
 package pl.akademiakodu.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.akademiakodu.data.GifRepository;
 import pl.akademiakodu.model.Gif;
 
 @Controller
 public class GifController {
+@Autowired
+GifRepository gifRepository;
 
 
 
     @RequestMapping("/")
 
 
-    public String listGifs(){
-
+    public String listGifs(ModelMap modelMap){
+modelMap.addAttribute("gifs",gifRepository.getAllGifs());
         return "/home";
 
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap){
-        Gif gif = new Gif("compiler-bot","michalos" , true);
+    @RequestMapping("/search")
+    public String searchGif(@RequestParam String q, ModelMap modelMap){
+        Gif gif = gifRepository.findByName(q);
+        if (gif == null)
+            return "redirect:/";
         modelMap.put("gif",gif);
         return "gif-details";
+    }
+
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        System.out.println(name);
+        modelMap.put("gif",gifRepository.findByName(name));
+        return "favorites";
     }
 
 }
--- src/main/java/pl/akademiakodu/data/GifRepository.java
+++ src/main/java/pl/akademiakodu/data/GifRepository.java
@@ -1,11 +1,12 @@
 package pl.akademiakodu.data;
 
+import org.springframework.stereotype.Component;
 import pl.akademiakodu.model.Gif;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Component
 public class GifRepository {
 
 
@@ -19,4 +20,15 @@ public class GifRepository {
             new Gif("infinite-andrew","andrew",true)
 
     );
+
+    public Gif findByName(String name){
+for (Gif gif: ALL_GIFS){
+    if (gif.getName().equals(name))
+        return gif;
+}return null;
+    }
+
+    public List<Gif> getAllGifs() {
+        return ALL_GIFS;
+    }
 }
